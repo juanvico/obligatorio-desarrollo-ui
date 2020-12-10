b0a2a66059f6016505cda51f4635678bef276017
Paola Francescoli <paola.francescoli@tigerspike.com>
2020-12-10 14:20:17 -0300

    Changes in UI

--- frontend/src/components/MessagesContainer.js
+++ frontend/src/components/MessagesContainer.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
+import { Typography } from '@material-ui/core';
 
 import MESSAGES from '../queries/messages';
 import { useQuery } from '@apollo/client';
@@ -38,17 +35,16 @@ const MessagesContainer = () => {
 
   return (
     <div className={classes.root}>
-      Mis mensajes:
+      My messages:
       <Grid container spacing={4}>
       {data?.items?.map((tile) => (
           <Grid item key={tile} xs={12} sm={6} md ={4}>
-            <Card className={classes.card}>
-              <CardActionArea>
-              <CardContent>
-                {tile.details}
-              </CardContent>
-              </CardActionArea>
-            </Card>
+            <Typography component="h1" variant="h5">
+            From: {tile.user_name} ( {tile.user_email} )
+			      </Typography> 
+            <Typography component="subtitle2" variant="subtitle2">
+            Message: {tile.details}
+			      </Typography> 
             </Grid>
       ))}
       </Grid>
