b4189c67baee595844aafd802a9544d38012a498
Paola Francescoli <paola.francescoli@tigerspike.com>
2020-12-15 11:54:39 -0300

    Create item

--- SwapApp/src/controllers/ItemController.js
+++ SwapApp/src/controllers/ItemController.js
@@ -3,6 +3,7 @@ import CREATE_ITEM from '_apollo/mutations/item';
 
 class ItemController {
   static createItem = async ({ title, description, image , pickupLatitude, pickupLongitude, pickupLocation, availableToPickup}) => {
+    console.log(title);
     const { data } = await apolloClient.mutate({
       mutation: CREATE_ITEM,
       variables: { title, description, image , pickupLatitude, pickupLongitude, pickupLocation, availableToPickup },
